24272aceb2c51c23e989ccf773ac8b354ad8a24a
Caio <bladellano@gmail.com>
2024-01-09 14:50:17 +0000

    :fire:Finalizando parceiros com mapa para Xingu

--- web/themes/custom/xinguBootstrap/lib/main.js
+++ web/themes/custom/xinguBootstrap/lib/main.js
@@ -7,15 +7,35 @@
             $('.loader_bg').fadeToggle();
         }, 2000);
 
-        // Carousel
-        const $carousel = $('.default_group');
+        // Flickity Carousel Observatorio
+        if (document.querySelector('#block_publicacoes')) {
 
-        $carousel.flickity();
+            const $carousel = $('#block_publicacoes .default_group');
 
-        $('#block_publicacoes .carousel-indicators').on('click', '.button', function () {
-            const index = $(this).index();
-            $carousel.flickity('select', index);
+            $carousel.flickity();
+
+            $('#block_publicacoes .carousel-indicators').on('click', '.button', function () {
+                const index = $(this).index();
+                $carousel.flickity('select', index);
+            });
+        }
+
+        // Popover for Xingu
+        $('[data-bs-toggle="popover"]').popover({
+            html: true,
+            content: function(e) {
+                return $('.popover-markup-' + e.dataset.id).html();
+            },
+            trigger: 'hover',
+            placement: 'top',
+            container: 'body',
         });
+
+        $('.mapitemlist > a').hover(
+            function (e) { $('.map-popover[data-id="' + e.currentTarget.dataset.id + '"]').popover('show') },
+            function (e) { $('.map-popover[data-id="' + e.currentTarget.dataset.id + '"]').popover('hide') }
+        );
+        // ./ End popover
 
     })
 
